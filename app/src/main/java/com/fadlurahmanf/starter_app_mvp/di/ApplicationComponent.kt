d28fe872067a7c21985a34fccce5e3699b872cf0
tffaj <taufik.fajari@akarinti.tech>
2021-09-30 08:29:56 +0700

    create application component for dagger

---
+++ app/src/main/java/com/fadlurahmanf/starter_app_mvp/di/ApplicationComponent.kt
@@ -0,0 +1,18 @@
+package com.fadlurahmanf.starter_app_mvp.di
+
+import android.app.Application
+import com.fadlurahmanf.starter_app_mvp.BaseApp
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = []) // TODO: 30/09/2021 INPUT YOUR MODULES HERE
+interface ApplicationComponent {
+    fun inject(baseApp: BaseApp)
+
+    @Component.Builder
+    interface Builder{
+        fun application(application: Application):Builder
+        fun build():ApplicationComponent
+    }
+}
