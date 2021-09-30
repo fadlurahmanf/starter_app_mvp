81b82b75875595633bbc366e0f9d7eff0c1b8114
tffaj <taufik.fajari@akarinti.tech>
2021-09-30 08:00:37 +0700

    delete message dummy

--- app/src/main/java/com/fadlurahmanf/starter_app_mvp/MainActivity.kt
+++ app/src/main/java/com/fadlurahmanf/starter_app_mvp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.fadlurahmanf.starter_app_mvp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
+//todo delete dummy
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
