36c46f74a6a088eac88a577a9d2bd80a4f22d401
tffaj <taufik.fajari@akarinti.tech>
2021-09-30 07:52:57 +0700

    initial message dummy

--- app/src/main/java/com/fadlurahmanf/starter_app_mvp/MainActivity.kt
+++ app/src/main/java/com/fadlurahmanf/starter_app_mvp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fadlurahmanf.starter_app_mvp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// TODO: 30/09/2021 TESS
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
